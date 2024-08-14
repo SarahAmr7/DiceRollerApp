4494de6ca57dab0c11d6f67cc0c7c9b4095c1958
SarahAmr <sab@blueribbon-inc.com>
2024-08-14 14:38:49 +0300

    Initial commit

---
+++ lib/dice_roller.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/dice-$currentDiceRoll.png', width: 200),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              //padding: const EdgeInsets.only(top: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 28,
+              )),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
